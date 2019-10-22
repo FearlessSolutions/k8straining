2ec1537c3cf60e34397ca322dd2ac85cc826ab4c
mkantzerFearless <mkantzer@fearless.tech>
2019-10-22 11:39:40 -0400

    initalize frontend folder and opts systems

--- helpers/buildandpush.sh
+++ helpers/buildandpush.sh
@@ -6,8 +6,10 @@ docker tag k8straining_add us.gcr.io/fearless-sandbox-2019/k8straining_add:lates
 docker tag k8straining_multiply us.gcr.io/fearless-sandbox-2019/k8straining_multiply:latest
 docker tag k8straining_nest1 us.gcr.io/fearless-sandbox-2019/k8straining_nest1:latest
 docker tag k8straining_nest2 us.gcr.io/fearless-sandbox-2019/k8straining_nest2:latest
+docker tag k8straining_nest2 us.gcr.io/fearless-sandbox-2019/k8straining_frontend:latest
 
 docker push us.gcr.io/fearless-sandbox-2019/k8straining_add:latest
 docker push us.gcr.io/fearless-sandbox-2019/k8straining_multiply:latest
 docker push us.gcr.io/fearless-sandbox-2019/k8straining_nest1:latest
 docker push us.gcr.io/fearless-sandbox-2019/k8straining_nest2:latest
+docker push us.gcr.io/fearless-sandbox-2019/k8straining_frontend:latest
